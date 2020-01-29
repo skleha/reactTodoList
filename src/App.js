4ad9fc65fbf717a51eec163eab762f317fd5bae7
thejenkdog <thejenkdog@gmail.com>
2020-01-29 14:03:12 -0800

    Change functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-
-  return (
-    <div>
-      <h1>Task List</h1>
-
-      <form>
-        <input
-          type="text"
-          placeholder="whaddya gotta do?">
-        </input>
-
-        <input
-          type="submit"
-          value="Click to Add">
-        </input>
-      
-      </form>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      todoItems: []
+    }
+  }
 
 
-    </div>
-  );
+  
+
+
+  render() {
+    return (
+      <div>
+        <h1>Task List</h1>
+
+        <form>
+          <input
+            type="text"
+            placeholder="whaddya gotta do?">
+          </input>
+
+          <input
+            type="submit"
+            value="Click to Add">
+          </input>
+        
+        </form>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
