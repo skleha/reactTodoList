80ec443d3ad6b150ee0d8dbf94f5fac2dbf11437
thejenkdog <thejenkdog@gmail.com>
2020-01-29 14:52:17 -0800

    Add handleInput function and new part of state

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  
   constructor(props) {
     super(props);
     this.state= {
+      currentTodo: "",
       todoItems: []
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value});
     }
   }
 
-  handleInput(item) {
-    return(e) => {
-      this.state.todoItems.push(e.target.value);
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.state.todoItems.push(this.state.currentTodo);
+    this.setState({currentTodo: ""});
   }
   
   render() {
@@ -21,10 +33,29 @@ class App extends React.Component {
         <h1>Add Task</h1>
 
         <form>
-          <input type="text" placeholder="whaddya gotta do?"></input>
+          <input
+            type="text"
+            placeholder="whaddya gotta do?"
+            onChange={this.handleChange('currentTodo')}>
+          </input>
 
-          <input type="submit" value="Click to Add"></input>
+          <input
+            type="submit"
+            value="Click to Add"
+            onClick={this.handleSubmit}>
+          </input>
+
         </form>
+
+        <h1>All Tasks:</h1>
+
+        <ul>
+          {this.state.todoItems.map((item, idx) => {
+            return <li key={idx}>{item}</li>
+          })}
+
+        </ul>
+
 
       </div>
     );
